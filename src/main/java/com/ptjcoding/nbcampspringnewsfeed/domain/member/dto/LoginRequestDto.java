6ccd5a3a3a35b7295df93d3c195ae51975ac9a90
gloz0315 <qudtn0315@naver.com>
2024-02-08 08:57:49 +0900

    ✨ feat(member): add LoginRequestDto class

---
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/dto/LoginRequestDto.java
@@ -0,0 +1,9 @@
+package com.ptjcoding.nbcampspringnewsfeed.domain.member.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginRequestDto {
+  private String email;
+  private String password;
+}
