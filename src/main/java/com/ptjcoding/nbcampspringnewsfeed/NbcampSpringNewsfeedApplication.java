e32f4f723daafb6c53fb782e6353d45b48fc5e0a
Ruh0n <wns8256@naver.com>
2024-02-07 16:05:32 +0900

    :tada: initial commit

---
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/NbcampSpringNewsfeedApplication.java
@@ -0,0 +1,13 @@
+package com.ptjcoding.nbcampspringnewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampSpringNewsfeedApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NbcampSpringNewsfeedApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ptjcoding/nbcampspringnewsfeed/NbcampSpringNewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptjcoding.nbcampspringnewsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcampSpringNewsfeedApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
