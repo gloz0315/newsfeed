91292f36e097a730e6a87d97aba35c98e17c0e71
devJWL <jwlee4450@gmail.com>
2024-02-07 17:58:37 +0900

    ✨ feat: add `ok` method

--- src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/common/dto/CommonResponseDto.java
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/common/dto/CommonResponseDto.java
@@ -1,11 +1,10 @@
 package com.ptjcoding.nbcampspringnewsfeed.domain.common.dto;
 
-import org.springframework.http.HttpStatus;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 @AllArgsConstructor
@@ -14,6 +13,11 @@ public class CommonResponseDto<T> {
 	private HttpStatus status;
 	private String message;
 	private T data;
+
+	public static <T> ResponseEntity<CommonResponseDto<T>> ok(String message, T data){
+		return ResponseEntity
+				.ok(new CommonResponseDto<>(HttpStatus.OK, message, data));
+	}
 
 	public String getStatus() {
 		return status.toString();
