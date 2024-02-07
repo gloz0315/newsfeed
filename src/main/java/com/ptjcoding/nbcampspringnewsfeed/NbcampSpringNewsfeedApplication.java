3308ed1359837340a248005db64ef72d12ba945d
Ruh0n <wns8256@naver.com>
2024-02-07 17:12:32 +0900

    ✨ feat: add auditing Timestamp class

--- src/main/java/com/ptjcoding/nbcampspringnewsfeed/NbcampSpringNewsfeedApplication.java
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/NbcampSpringNewsfeedApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing()
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @PropertySource(value = {"classpath:db.properties"})
 public class NbcampSpringNewsfeedApplication {
