1fc95e72a9b37a62db912a3415fc66be2ff1a2f5
gloz0315 <qudtn0315@naver.com>
2024-02-08 20:59:48 +0900

    🔥 remove(member): remove not needed code

--- src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/model/Member.java
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/model/Member.java
@@ -17,24 +17,4 @@ public class Member {
   private String password;
   private MemberRole role;
 
-  public Member update(String nickname, String password) {
-    if (!this.password.equals(password)) {
-      throw new IllegalArgumentException("비밀번호가 맞지 않습니다.");
-    }
-
-    return Member.builder()
-        .id(id)
-        .email(email)
-        .nickname(nickname)
-        .password(password)
-        .role(role)
-        .build();
-  }
-
-  // TODO: 회원 탈퇴에 쓰일 메서드
-  public void delete(String password) {
-    if (!this.password.equals(password)) {
-      throw new IllegalArgumentException("비밀번호가 맞지 않습니다.");
-    }
-  }
 }
