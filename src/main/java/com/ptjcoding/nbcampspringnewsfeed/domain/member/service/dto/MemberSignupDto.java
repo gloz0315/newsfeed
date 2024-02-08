00992edeef77586b6194642c61024e8a6df5fe63
gloz0315 <qudtn0315@naver.com>
2024-02-08 16:10:48 +0900

    ✨ feat(member): add MemberResponseDto, MemberSignupDto

---
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/service/dto/MemberSignupDto.java
@@ -0,0 +1,26 @@
+package com.ptjcoding.nbcampspringnewsfeed.domain.member.service.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberSignupDto {
+
+  private final String email;
+  private final String nickname;
+  private final String password;
+  private final String checkPassword;
+
+  public MemberSignupDto(String email, String nickname, String password, String checkPassword) {
+    checkPasswordEquals(password, checkPassword);
+    this.email = email;
+    this.nickname = nickname;
+    this.password = password;
+    this.checkPassword = checkPassword;
+  }
+
+  private void checkPasswordEquals(String password, String checkPassword) {
+    if (!password.equals(checkPassword)) {
+      throw new IllegalArgumentException("비밀번호가 서로 맞지 않습니다.");
+    }
+  }
+}
