e64f205c3c0f65f6d9c1283964699741d0550acb
gloz0315 <qudtn0315@naver.com>
2024-02-08 09:11:15 +0900

    ✨ feat(member): add MemberService interface

---
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/service/MemberService.java
@@ -0,0 +1,17 @@
+package com.ptjcoding.nbcampspringnewsfeed.domain.member.service;
+
+import com.ptjcoding.nbcampspringnewsfeed.domain.common.dto.CommonResponseDto;
+import com.ptjcoding.nbcampspringnewsfeed.domain.member.dto.LoginRequestDto;
+import com.ptjcoding.nbcampspringnewsfeed.domain.member.dto.SignupRequestDto;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface MemberService {
+
+  @Transactional
+  ResponseEntity<CommonResponseDto<Void>> signup(
+      SignupRequestDto signupRequestDto);
+
+  ResponseEntity<CommonResponseDto<Void>> login(LoginRequestDto loginRequestDto, HttpServletResponse response);
+}
