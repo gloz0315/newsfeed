919a850ffd570f7426245b9b093930dd87aecbb3
devJWL <jwlee4450@gmail.com>
2024-02-08 18:10:09 +0900

    🎨 fix(post): change structure
    add field deleteDate
    change field created_date to createdDate
    change field updated_date to updatedDate
    add method from

--- src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/post/dto/PostResponseDto.java
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/post/dto/PostResponseDto.java
@@ -1,8 +1,10 @@
 package com.ptjcoding.nbcampspringnewsfeed.domain.post.dto;
 
-import com.ptjcoding.nbcampspringnewsfeed.domain.post.entity.Post;
+import com.ptjcoding.nbcampspringnewsfeed.domain.common.dto.CommonResponseDto;
+import com.ptjcoding.nbcampspringnewsfeed.domain.post.model.Post;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 public class PostResponseDto {
@@ -12,16 +14,22 @@ public class PostResponseDto {
   private final String content;
   private final Long agreeCount;
   private final Long disagreeCount;
-  private final LocalDateTime created_date;
-  private final LocalDateTime updated_date;
+  private final LocalDateTime createdDate;
+  private final LocalDateTime updatedDate;
+  private final LocalDateTime deleteDate;
 
-  public PostResponseDto(Post post, String nickname) {
-    this.nickname = nickname;
+  public PostResponseDto(Post post) {
+    this.nickname = post.getNickname();
     this.title = post.getTitle();
     this.content = post.getContent();
     this.agreeCount = post.getAgreeCount();
     this.disagreeCount = post.getDisagreeCount();
-    this.created_date = post.getCreatedDate();
-    this.updated_date = post.getUpdatedDate();
+    this.createdDate = post.getCreatedDate();
+    this.updatedDate = post.getUpdatedDate();
+    this.deleteDate = post.getDeletedDate();
+  }
+
+  public static ResponseEntity<CommonResponseDto<PostResponseDto>> from(PostResponseDto postResponseDto) {
+    return CommonResponseDto.ok("게시글 작성 성공", postResponseDto);
   }
 }
