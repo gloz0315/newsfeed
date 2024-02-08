dfb26aee9387a22b3476026c57d33bec499a83cf
gloz0315 <qudtn0315@naver.com>
2024-02-08 09:13:20 +0900

    ✨ fix(member): change parameter to MemberController
    - add parameter to login method

--- src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/controller/MemberController.java
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ptjcoding.nbcampspringnewsfeed.domain.member.controller;
 import com.ptjcoding.nbcampspringnewsfeed.domain.common.dto.CommonResponseDto;
 import com.ptjcoding.nbcampspringnewsfeed.domain.member.dto.LoginRequestDto;
 import com.ptjcoding.nbcampspringnewsfeed.domain.member.service.MemberServiceImpl;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +27,7 @@ public class MemberController {
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<CommonResponseDto<Void>> login(@Valid @RequestBody LoginRequestDto loginRequestDto) {
-		return memberService.login(loginRequestDto);
+	public ResponseEntity<CommonResponseDto<Void>> login(@Valid @RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+		return memberService.login(loginRequestDto,response);
 	}
 }
