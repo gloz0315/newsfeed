c6758d5b7c61a34c6268f4c729c264336c3ecdd2
Ruh0n <wns8256@naver.com>
2024-02-08 13:30:19 +0900

    ✨ feat(comment): implement CommentServiceImpl
    - createComment
    - updateComment
    - deleteComment

---
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/comment/service/CommentServiceImpl.java
@@ -0,0 +1,43 @@
+package com.ptjcoding.nbcampspringnewsfeed.domain.comment.service;
+
+import com.ptjcoding.nbcampspringnewsfeed.domain.comment.dto.CommentRequestDto;
+import com.ptjcoding.nbcampspringnewsfeed.domain.comment.model.Comment;
+import com.ptjcoding.nbcampspringnewsfeed.domain.comment.repository.CommentRepositoryImpl;
+import com.ptjcoding.nbcampspringnewsfeed.domain.comment.repository.dto.CommentCreateDto;
+import com.ptjcoding.nbcampspringnewsfeed.domain.comment.repository.dto.CommentUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+// ! TODO: need to verify member before repository calls
+
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService {
+
+  private final CommentRepositoryImpl commentRepository;
+
+  @Override
+  public Comment createComment(CommentRequestDto requestDto) {
+    CommentCreateDto createDto = CommentCreateDto.builder()
+        .content(requestDto.getContent())
+// !    .memberId(member.getMemberId())                                // TODO: need to add memberId
+        .postId(requestDto.getPostId())
+        .parentCommentId(requestDto.getParentCommentId())
+        .build();
+
+    return commentRepository.createComment(createDto);
+  }
+
+  @Override
+  public Comment updateComment(Long commentId, CommentRequestDto requestDto) {
+    CommentUpdateDto updateDto = CommentUpdateDto.of(requestDto);
+
+    return commentRepository.updatecomment(commentId, updateDto);
+  }
+
+  @Override
+  public void deleteComment(Long commentId) {
+    commentRepository.deleteById(commentId);
+  }
+
+}
