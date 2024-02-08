3d58e142c97677c1a63950d927e42829bcddbb1c
gloz0315 <qudtn0315@naver.com>
2024-02-08 15:56:36 +0900

    🎨 fix(member): change class name
    - add @SQLDelete annotation
    - add @SQLRestriction annotation
    - add methods

---
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/infrastructure/entity/MemberEntity.java
@@ -0,0 +1,67 @@
+package com.ptjcoding.nbcampspringnewsfeed.domain.member.infrastructure.entity;
+
+import com.ptjcoding.nbcampspringnewsfeed.domain.common.entity.Timestamped;
+import com.ptjcoding.nbcampspringnewsfeed.domain.member.model.Member;
+import com.ptjcoding.nbcampspringnewsfeed.domain.member.model.MemberRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "member")
+@SQLDelete(sql = "update member set deleted_date = NOW() where id = ?")
+@SQLRestriction(value = "deleted_date is NULL")
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberEntity extends Timestamped {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true, length = 40)
+  private String email;
+
+  @Column(nullable = false, length = 15)
+  private String nickname;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false, length = 10)
+  @Enumerated(value = EnumType.STRING)
+  private MemberRole role;
+
+  public static MemberEntity of(String email, String nickname, String password, MemberRole role) {
+
+    return MemberEntity.builder()
+        .email(email)
+        .nickname(nickname)
+        .password(password)
+        .role(role)
+        .build();
+  }
+
+  public Member toModel() {
+    return Member.builder()
+        .id(id)
+        .email(email)
+        .nickname(nickname)
+        .password(password)
+        .role(role)
+        .build();
+  }
+}
