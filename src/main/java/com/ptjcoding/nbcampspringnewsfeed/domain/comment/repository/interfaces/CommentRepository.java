f8b45ed8c21fb8a1c2e2fc99269e0a8055d3e4c1
Ruh0n <wns8256@naver.com>
2024-02-09 17:36:46 +0900

    ✨ feat(comment): implement method to get comments by postId

--- src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/comment/repository/interfaces/CommentRepository.java
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/comment/repository/interfaces/CommentRepository.java
@@ -3,13 +3,15 @@ package com.ptjcoding.nbcampspringnewsfeed.domain.comment.repository.interfaces;
 import com.ptjcoding.nbcampspringnewsfeed.domain.comment.model.Comment;
 import com.ptjcoding.nbcampspringnewsfeed.domain.comment.repository.dto.CommentCreateDto;
 import com.ptjcoding.nbcampspringnewsfeed.domain.comment.repository.dto.CommentUpdateDto;
+import java.util.List;
 
 public interface CommentRepository {
 
   Comment createComment(CommentCreateDto dto);
 
+  List<Comment> getCommentsByPostId(Long postId);
+
   Comment updatecomment(long id, CommentUpdateDto dto);
 
   void deleteById(long id);
-
 }
--- src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/comment/service/CommentService.java
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/comment/service/CommentService.java
@@ -3,10 +3,13 @@ package com.ptjcoding.nbcampspringnewsfeed.domain.comment.service;
 import com.ptjcoding.nbcampspringnewsfeed.domain.comment.dto.CommentCreateRequestDto;
 import com.ptjcoding.nbcampspringnewsfeed.domain.comment.dto.CommentUpdateRequestDto;
 import com.ptjcoding.nbcampspringnewsfeed.domain.comment.model.Comment;
+import java.util.List;
 
 public interface CommentService {
 
   Comment createComment(CommentCreateRequestDto requestDto);
+
+  List<Comment> getCommentsByPostId(Long postId);
 
   Comment updateComment(Long commentId, CommentUpdateRequestDto requestDto);
 
