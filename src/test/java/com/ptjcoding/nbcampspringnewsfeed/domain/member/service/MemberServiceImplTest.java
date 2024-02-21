e80bd7f19d4ff4346209fe41a37090889f606f7c
gloz0315 <qudtn0315@naver.com>
2024-02-21 21:09:04 +0900

    ✨ feat(member): memberServiceTest methods

--- src/test/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/service/MemberServiceImplTest.java
+++ src/test/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/service/MemberServiceImplTest.java
@@ -2,21 +2,23 @@ package com.ptjcoding.nbcampspringnewsfeed.domain.member.service;
 
 import static com.ptjcoding.nbcampspringnewsfeed.test.TestInfo.CHANGE_NICKNAME;
 import static com.ptjcoding.nbcampspringnewsfeed.test.TestInfo.TEST_EMAIL;
+import static com.ptjcoding.nbcampspringnewsfeed.test.TestInfo.TEST_FAIL_NICKNAME;
 import static com.ptjcoding.nbcampspringnewsfeed.test.TestInfo.TEST_ID;
 import static com.ptjcoding.nbcampspringnewsfeed.test.TestInfo.TEST_NICKNAME;
 import static com.ptjcoding.nbcampspringnewsfeed.test.TestInfo.TEST_PASSWORD;
 import static com.ptjcoding.nbcampspringnewsfeed.test.TestInfo.TEST_ROLE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
 
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
+import com.navercorp.fixturemonkey.jakarta.validation.plugin.JakartaValidationPlugin;
 import com.ptjcoding.nbcampspringnewsfeed.domain.blacklist.repository.BlackListRepository;
-import com.ptjcoding.nbcampspringnewsfeed.domain.bookmark.repository.BookmarkRepository;
-import com.ptjcoding.nbcampspringnewsfeed.domain.comment.repository.interfaces.CommentRepository;
-import com.ptjcoding.nbcampspringnewsfeed.domain.hall_of_fame.repository.HallOfFameRepository;
 import com.ptjcoding.nbcampspringnewsfeed.domain.member.dto.LoginRequestDto;
 import com.ptjcoding.nbcampspringnewsfeed.domain.member.dto.NicknameUpdateRequestDto;
 import com.ptjcoding.nbcampspringnewsfeed.domain.member.dto.SignupRequestDto;
@@ -25,8 +27,6 @@ import com.ptjcoding.nbcampspringnewsfeed.domain.member.repository.MemberReposit
 import com.ptjcoding.nbcampspringnewsfeed.domain.member.repository.dto.NicknameUpdateDto;
 import com.ptjcoding.nbcampspringnewsfeed.domain.member.service.dto.MemberResponseDto;
 import com.ptjcoding.nbcampspringnewsfeed.domain.member.service.dto.NicknameChangeDto;
-import com.ptjcoding.nbcampspringnewsfeed.domain.post.repository.PostRepository;
-import com.ptjcoding.nbcampspringnewsfeed.domain.vote.repository.interfaces.VoteRepository;
 import com.ptjcoding.nbcampspringnewsfeed.global.jwt.JwtProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -35,7 +35,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -47,19 +46,14 @@ class MemberServiceImplTest {
   @Mock
   private MemberRepository memberRepository;
   @Mock
-  private PostRepository postRepository;
-  @Mock
-  private CommentRepository commentRepository;
-  @Mock
-  private VoteRepository voteRepository;
-  @Mock
-  private BookmarkRepository bookmarkRepository;
-  @Mock
-  private HallOfFameRepository hallOfFameRepository;
-  @Mock
   private BlackListRepository blackListRepository;
   @Mock
   private JwtProvider jwtProvider;
+
+  private final FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
+      .plugin(new JakartaValidationPlugin())
+      .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE)// 이거는 필드리플렉션 인스턴스를 설정해주는 것
+      .build();
 
   @InjectMocks
   private MemberServiceImpl memberService;
@@ -145,8 +139,9 @@ class MemberServiceImplTest {
           .role(TEST_ROLE)
           .build();
 
-      NicknameUpdateRequestDto dto = new NicknameUpdateRequestDto();
-      ReflectionTestUtils.setField(dto, "nickname", CHANGE_NICKNAME);
+      NicknameUpdateRequestDto dto = fixtureMonkey.giveMeBuilder(NicknameUpdateRequestDto.class)
+          .set("nickname", CHANGE_NICKNAME)
+          .sample();
 
       Member changedMember = Member.builder()
           .nickname(CHANGE_NICKNAME)
@@ -163,7 +158,6 @@ class MemberServiceImplTest {
 
   }
 
-
   @Nested
   @DisplayName("회원 실패 테스트")
   class FailTest {
@@ -172,16 +166,19 @@ class MemberServiceImplTest {
     @DisplayName("회원 중복 가입 실패 테스트")
     void test1() {
       // given
-      Member member = Member.builder().id(TEST_ID)
-          .email(TEST_EMAIL)
-          .nickname(TEST_NICKNAME)
-          .password(TEST_PASSWORD)
-          .role(TEST_ROLE).build();
-
+      SignupRequestDto dto = fixtureMonkey.giveMeBuilder(SignupRequestDto.class)
+          .validOnly(false)
+          .set("email",TEST_EMAIL)
+          .set("nickname", TEST_FAIL_NICKNAME)
+          .set("password",TEST_PASSWORD)
+          .set("checkPassword",TEST_PASSWORD)
+          .sample();
 
       // when
+      given(memberRepository.checkEmail(TEST_EMAIL)).willReturn(true);
 
       // then
+      assertThrows(Exception.class, () -> memberService.signup(dto));
     }
   }
 
