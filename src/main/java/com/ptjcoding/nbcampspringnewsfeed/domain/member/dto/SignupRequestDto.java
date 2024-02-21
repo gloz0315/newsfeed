3c80c4684e39157a5528263c399f768f0f1c7497
gloz0315 <qudtn0315@naver.com>
2024-02-21 21:08:43 +0900

    ✨ feat(member): add SignupRequestDto methods

--- src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/dto/SignupRequestDto.java
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/domain/member/dto/SignupRequestDto.java
@@ -3,6 +3,7 @@ package com.ptjcoding.nbcampspringnewsfeed.domain.member.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import java.util.Objects;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -32,4 +33,21 @@ public class SignupRequestDto {
   @Pattern(regexp = "^(?=.*[a-z]).+$")
   @Pattern(regexp = "^(?=.*[A-Z]).+$")
   private String checkPassword;
+
+  @Override
+  public boolean equals(Object o) {
+    if(this == o) return true;
+    if(o == null || getClass() != o.getClass()) return false;
+
+    SignupRequestDto dto = (SignupRequestDto) o;
+    return Objects.equals(email, dto.email) &&
+        Objects.equals(nickname, dto.nickname) &&
+        Objects.equals(password, dto.password) &&
+        Objects.equals(checkPassword, dto.checkPassword);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(email, nickname, password, checkPassword);
+  }
 }
