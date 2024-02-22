2d753661396d373c97a355bc171b6f5b26cfdeef
gloz0315 <qudtn0315@naver.com>
2024-02-22 09:02:43 +0900

    ✨ feat(webConfig): add h2 database

--- src/main/java/com/ptjcoding/nbcampspringnewsfeed/global/config/WebConfig.java
+++ src/main/java/com/ptjcoding/nbcampspringnewsfeed/global/config/WebConfig.java
@@ -28,7 +28,8 @@ public class WebConfig implements WebMvcConfigurer {
                         "/api/**/members/signup",
                         "/swagger-ui/**",
                         "/swagger-ui.html",
-                        "/v3/api-docs/**"
+                        "/v3/api-docs/**",
+                        "/h2-console/**"
                 );
     }
 }
